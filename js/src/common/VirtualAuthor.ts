a502bc9c547c7115fd1a5d0153562b9992b4c0d0
David Wheatley <hi@davwheat.dev>
2021-11-25 01:13:26 +0000

    feat: add credit attribute helper

--- js/src/common/VirtualAuthor.ts
+++ js/src/common/VirtualAuthor.ts
@@ -3,6 +3,7 @@ import Model from 'flarum/common/Model';
 export default class VirtualAuthor extends Model {
   displayName = Model.attribute('displayName');
   description = Model.attribute('description');
+  credit = Model.attribute('credit');
 
   creditedDiscussions = Model.hasMany('discussions');
 }

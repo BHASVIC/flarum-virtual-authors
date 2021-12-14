a1d0a1b6c81a5389f743a665b456c22175a0faab
Ian Morland <ian@morland.me>
2021-12-14 20:38:03 +0000

    Remove gambit stuff

--- src/Search/VirtualAuthorDisplayNameGambit.php
+++ src/Search/VirtualAuthorDisplayNameGambit.php
@@ -4,22 +4,10 @@ namespace Davwheat\VirtualAuthors\Search;
 
 use Flarum\Filter\FilterInterface;
 use Flarum\Filter\FilterState;
-use Flarum\Search\AbstractRegexGambit;
-use Flarum\Search\SearchState;
 use Illuminate\Database\Query\Builder;
 
-class VirtualAuthorDisplayNameGambit extends AbstractRegexGambit implements FilterInterface
+class VirtualAuthorDisplayNameGambit implements FilterInterface
 {
-    protected function getGambitPattern()
-    {
-        return 'displayName:\"(.+)\"';
-    }
-
-    protected function conditions(SearchState $search, array $matches, $negate)
-    {
-        $this->constrain($search->getQuery(), $matches[1], $negate);
-    }
-
     public function getFilterKey(): string
     {
         return 'displayName';
@@ -32,6 +20,6 @@ class VirtualAuthorDisplayNameGambit extends AbstractRegexGambit implements Filt
 
     protected function constrain(Builder $query, $name, $negate)
     {
-        $query->where('display_name', 'like', '%' . $name . '%', 'or');
+        $query->where('display_name', 'like', '%' . $name . '%', 'or', $negate);
     }
 }
